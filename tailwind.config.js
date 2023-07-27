b17c5ec1c1578e750effdbdccea0e5bb1fa02971
Edoitachime <96532589+edoitachi@users.noreply.github.com>
2023-07-27 15:28:55 +0500

    fix bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 // tailwind.config.js
 module.exports = {
-	content: [
-		"./index.html",
-		"./src/**/*.{js,ts,jsx,tsx}",
-		"./node_modules/flowbite/**/*.js",
-	],
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
 			colors: {
@@ -27,5 +23,5 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [require("flowbite/plugin")],
+	plugins: [],
 }
